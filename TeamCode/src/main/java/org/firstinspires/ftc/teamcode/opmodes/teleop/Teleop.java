b4084ec052e639c663132153ab22ef04540f78f6
Javier <javier.huang168@gmail.com>
2023-11-07 15:38:35 -0500

    pushed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Teleop.java
@@ -39,24 +39,23 @@ public class Teleop extends BaseTeleop {
 
     public void controlGP1() {
         double lx = gamepad1.left_stick_x, ly = gamepad1.left_stick_y, rx = gamepad1.right_stick_x, ry = gamepad1.right_stick_y;
-        double lxp = Math.pow(lx, 2), lyp = Math.pow(ly, 2), rxp = Math.pow(rx, 2), ryp = Math.pow(ry, 2);
         boolean a = gamepad1.a, b = gamepad1.b, x = gamepad1.x, y = gamepad1.y;
         if (Chassis.TANK_DRIVE) {
             // Tank drive
-            chassis.fl.setPower(MathHelper.deadzone(lyp, JOYSTICK_DEADZONE));
-            chassis.fr.setPower(MathHelper.deadzone(ryp, JOYSTICK_DEADZONE));
+            chassis.fl.setPower(MathHelper.deadzone(ly, JOYSTICK_DEADZONE));
+            chassis.fr.setPower(MathHelper.deadzone(ry, JOYSTICK_DEADZONE));
             // two wheeled
-            if (Chassis.TWO_WHEELED) {
-                chassis.bl.setPower(MathHelper.deadzone(lyp, JOYSTICK_DEADZONE));
-                chassis.br.setPower(MathHelper.deadzone(ryp, JOYSTICK_DEADZONE));
+            if (!Chassis.TWO_WHEELED) {
+                chassis.bl.setPower(MathHelper.deadzone(ly, JOYSTICK_DEADZONE));
+                chassis.br.setPower(MathHelper.deadzone(ry, JOYSTICK_DEADZONE));
             }
         } else {
             // Arcade drive
-            chassis.fl.setPower(MathHelper.deadzone(lyp - rxp, JOYSTICK_DEADZONE));
-            chassis.fr.setPower(MathHelper.deadzone(lyp + rxp, JOYSTICK_DEADZONE));
-            if (Chassis.TWO_WHEELED) {
-                chassis.bl.setPower(MathHelper.deadzone(lyp - rxp, JOYSTICK_DEADZONE));
-                chassis.br.setPower(MathHelper.deadzone(lyp + rxp, JOYSTICK_DEADZONE));
+            chassis.fl.setPower(MathHelper.deadzone(ly - rx, JOYSTICK_DEADZONE));
+            chassis.fr.setPower(MathHelper.deadzone(ly + rx, JOYSTICK_DEADZONE));
+            if (!Chassis.TWO_WHEELED) {
+                chassis.bl.setPower(MathHelper.deadzone(ly - rx, JOYSTICK_DEADZONE));
+                chassis.br.setPower(MathHelper.deadzone(ly + rx, JOYSTICK_DEADZONE));
             }
         }
     }
@@ -65,35 +64,49 @@ public class Teleop extends BaseTeleop {
         double lx = gamepad2.left_stick_x, ly = gamepad2.left_stick_y, rx = gamepad2.right_stick_x, ry = gamepad2.right_stick_y;
         double lxp = Math.pow(lx, 2), lyp = Math.pow(ly, 2), rxp = Math.pow(rx, 2), ryp = Math.pow(ry, 2);
         boolean a = gamepad2.a, b = gamepad2.b, x = gamepad2.x, y = gamepad2.y;
+        if (Chassis.HAS_PIVOT) {
+            chassis.pivot.setPower(lx);
+        }
         if (Chassis.HAS_ROLLER) {
             chassis.roller.setPower(ly);
         }
-        if (path == null) {
-            ArrayList<AutoPoint> armPoints = new ArrayList<>();
-            ArrayList<AutoAction> armActions = new ArrayList<>();
-            // lift arm
-            if (a) {
-                AutoPathHelper.addArmUpMovement(chassis, armActions);
-            }
-            // lower arm
-            if (b) {
-                AutoPathHelper.addArmDownMovement(chassis, armActions);
-            }
-            // drop pixel
-            if (x) {
-                AutoPathHelper.addFlapOpenMovement(chassis, armActions);
-                armActions.add(new WaitAction(chassis, 1000));
-                AutoPathHelper.addFlapCloseMovement(chassis, armActions);
-            }
-            armPoints.add(new AutoPoint(new Point(0, 0), armActions));
-            path = new AutoPath(chassis, armPoints, false);
+        if (Chassis.HAS_ARM) {
+            chassis.arm.setPower(ry);
         }
+        if (Chassis.HAS_FLAP) {
+            if (rx > 0.9) {
+                chassis.flap.setPosition(1);
+            } else {
+                chassis.flap.setPosition(0);
+            }
+        }
+//        if (path == null) {
+//            ArrayList<AutoPoint> armPoints = new ArrayList<>();
+//            ArrayList<AutoAction> armActions = new ArrayList<>();
+//            // lift arm
+//            if (a) {
+//                AutoPathHelper.addArmUpMovement(chassis, armActions);
+//            }
+//            // lower arm
+//            if (b) {
+//                AutoPathHelper.addArmDownMovement(chassis, armActions);
+//            }
+//            // drop pixel
+//            if (x) {
+//                AutoPathHelper.addFlapOpenMovement(chassis, armActions);
+//                armActions.add(new WaitAction(chassis, 1000));
+//                AutoPathHelper.addFlapCloseMovement(chassis, armActions);
+//            }
+//            armPoints.add(new AutoPoint(new Point(0, 0), armActions, 0));
+//            armPoints.add(new AutoPoint(new Point(0, 0), new ArrayList<>(), 0));
+//            path = new AutoPath(chassis, armPoints, false);
+//        }
     }
 
     public void tick() {
-        if (path != null) {
-            path.tick();
-            if (!path.active) path = null;
-        }
+//        if (path != null) {
+//            path.tick();
+//            if (!path.active) path = null;
+//        }
     }
 }
