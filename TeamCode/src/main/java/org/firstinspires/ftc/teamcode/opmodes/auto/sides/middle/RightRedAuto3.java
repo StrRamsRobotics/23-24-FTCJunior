2d3260622ce0f2aff32c618855c379ebe5edd20c
Javier <javier.huang168@gmail.com>
2023-12-03 14:49:22 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sides/middle/RightRedAuto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sides/middle/RightRedAuto3.java
@@ -42,13 +42,13 @@ public class RightRedAuto3 extends BaseAuto {
 
         switch(route) {
             case 0:
-                points.add(new AutoPoint(new Point(4.75 * Game.TILE_SIZE, 2.75 * Game.TILE_SIZE), purpleActions, true));
+                points.add(new AutoPoint(new Point(4.5 * Game.TILE_SIZE, 2.75 * Game.TILE_SIZE), purpleActions, true));
                 points.add(new AutoPoint(new Point(5.25 * Game.TILE_SIZE, 2.5 * Game.TILE_SIZE), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(4.25 * Game.TILE_SIZE, 1.25 * Game.TILE_SIZE), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(4.25 * Game.TILE_SIZE, 1.2 * Game.TILE_SIZE), yellowActions, false));
                 break;
             case 2:
-                points.add(new AutoPoint(new Point(4.75 * Game.TILE_SIZE, 2.25 * Game.TILE_SIZE), purpleActions, true));
+                points.add(new AutoPoint(new Point(4.5 * Game.TILE_SIZE, 2.25 * Game.TILE_SIZE), purpleActions, true));
                 points.add(new AutoPoint(new Point(5.25 * Game.TILE_SIZE, 2.5 * Game.TILE_SIZE), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(4.25 * Game.TILE_SIZE, 1.25 * Game.TILE_SIZE), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(4.25 * Game.TILE_SIZE, 1.2 * Game.TILE_SIZE), yellowActions, false));
