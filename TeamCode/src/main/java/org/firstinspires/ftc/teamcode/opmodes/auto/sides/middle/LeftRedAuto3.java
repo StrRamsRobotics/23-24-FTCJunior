fcc63747356e33b1bf96f5144fb1dbb1c381f45c
JavRedstone <javier.huang168@gmail.com>
2023-12-06 23:05:12 -0500

    update RB and LR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sides/middle/LeftRedAuto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sides/middle/LeftRedAuto3.java
@@ -60,8 +60,8 @@ public class LeftRedAuto3 extends BaseAuto {
             default:
                 points.add(new AutoPoint(new Point(6 * Game.TS - (2 * Game.TS - Chassis.HRW), 4.5 * Game.TS), purpleActions, true));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.5 * Game.TS, 4.5 * Game.TS), new ArrayList<>(), false));
-                points.add(new AutoPoint(new Point(6 * Game.TS - 1.5 * Game.TS, 5.5 * Game.TS), new ArrayList<>(), false));
-                points.add(new AutoPoint(new Point(6 * Game.TS - 2.5 * Game.TS, 5.5 * Game.TS), new ArrayList<>(), false));
+                points.add(new AutoPoint(new Point(6 * Game.TS - 1.5 * Game.TS, 5.25 * Game.TS), new ArrayList<>(), false));
+                points.add(new AutoPoint(new Point(6 * Game.TS - 2.5 * Game.TS, 5.25 * Game.TS), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 2.5 * Game.TS, 2 * Game.TS), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.25 * Game.TS, 0.5 * Game.TS + Chassis.HRW + 0.1), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.25 * Game.TS, 0.5 * Game.TS + Chassis.HRW), yellowActions, false));
