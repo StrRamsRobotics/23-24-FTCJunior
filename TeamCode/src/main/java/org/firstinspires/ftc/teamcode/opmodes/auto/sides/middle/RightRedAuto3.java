cb57cd119e1ea499ea251b12feb1e0ae692b1a8c
Javier <javier.huang168@gmail.com>
2023-12-07 15:52:16 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sides/middle/RightRedAuto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/sides/middle/RightRedAuto3.java
@@ -42,7 +42,7 @@ public class RightRedAuto3 extends BaseAuto {
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.5 * Game.TS, 2.5 * Game.TS), purpleActions, false));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.5 * Game.TS, 2 * Game.TS + Chassis.HRW), new ArrayList<>(), true));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.5 * Game.TS, 2.5 * Game.TS), new ArrayList<>(), true));
-                points.add(new AutoPoint(new Point(6 * Game.TS - 1.5 * Game.TS, 2.5 * Game.TS), new ArrayList<>(), false));
+                points.add(new AutoPoint(new Point(6 * Game.TS - 0.5 * Game.TS, 2.5 * Game.TS), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.75 * Game.TS, 0.5 * Game.TS + Chassis.HRW + Chassis.INTERMEDIATE_BACKBOARD), new ArrayList<>(), false));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.75 * Game.TS, 0.5 * Game.TS + Chassis.HRW), yellowActions, true));
                 points.add(new AutoPoint(new Point(6 * Game.TS - 1.75 * Game.TS, 0.5 * Game.TS + Chassis.HRW + Chassis.INTERMEDIATE_BACKBOARD), new ArrayList<>(), false));
