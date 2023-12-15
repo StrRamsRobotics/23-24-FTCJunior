35cc11ab0021394f29fad06fdca067bb62991c64
Ethan Xu <ethanxucoder@gmail.com>
2023-12-15 16:38:46 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/test/HangTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/test/HangTest.java
@@ -4,8 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.opmodes.base.BaseAuto;
 import org.firstinspires.ftc.teamcode.wrappers.Chassis;
 
+
 @Autonomous(name="HangTest")
 public class HangTest extends BaseAuto {
+    public static boolean LOOP_HAS_RUN = false;
+
     @Override
     public void runSetup() {
 
@@ -17,11 +20,22 @@ public class HangTest extends BaseAuto {
 
     @Override
     public void runLoop() {
-        if (Chassis.HAS_HANG) {
-            chassis.hang1.setPower(-1);
-            chassis.hang2.setPower(-1);
+        if (Chassis.HAS_HANG && !LOOP_HAS_RUN) {
+            chassis.hang1.setPower(-0.8);
+            chassis.hang2.setPower(-0.8);
             chassis.logHelper.addData("hang", "up");
             chassis.logHelper.update();
+            sleep(1000);
+            chassis.hang1.setPower(0.05);
+            chassis.hang2.setPower(0.05);
+            sleep(10000);
+            chassis.logHelper.addData("hang", "hover");
+            chassis.logHelper.update();
+            chassis.hang1.setPower(0.8);
+            chassis.hang2.setPower(0.8);
+            chassis.logHelper.addData("hang", "down");
+            chassis.logHelper.update();
+            LOOP_HAS_RUN = true;
         }
     }
 }
